d3589ce3761c95a0b46fab207de6fff8cd13a7ce
unknown <131908325+lng-ng@users.noreply.github.com>
2024-03-26 15:56:18 +0100

    add clarity to stratified sampling

--- stratified_sampling.py
+++ stratified_sampling.py
@@ -32,13 +32,14 @@ if __name__ == "__main__":
     save_path = os.path.join(datetime.utcnow().isoformat().replace(':', '_'))
     runtimes = []
     os.makedirs(save_path)
+    print(f"Output folder: {save_path}")
     for i in range(n):
         t = time.time()
         res_df = random_deletion(df, p, qids)
         t = time.time() - t
         runtimes.append(t)
         res_df.to_csv(os.path.join(save_path, f'{dataset_id}_sampled_p{int(p*100)}_t{i}.csv'),index=False)
-
+    print("Stratified sampling finished.")
     print(f"iteration,deletion_percentage,run_time")
     for i, t in enumerate(runtimes):
         print(i, p, t)
