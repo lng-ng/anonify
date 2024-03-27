97ef057501bf26f2a1cb000eba452a84f74ab43c
unknown <131908325+lng-ng@users.noreply.github.com>
2024-03-27 10:13:31 +0100

    fix gdown not finding file

--- utils/download.py
+++ utils/download.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     parser.add_argument("expr", help="experiment type [infoloss | dist | ml | anon]")
     args = parser.parse_args()
     if args.expr == 'infoloss':
-        gdown.download_folder(id=drive_ids['expr1_data'], quiet=False)
+        gdown.download_folder(drive_ids['expr1_data'], quiet=False)
     elif args.expr == 'dist':
         data_folder = 'dist_data'
         os.makedirs(data_folder, exist_ok=True)
-        gdown.download(id=drive_ids['original'][0], output=os.path.join(data_folder, drive_ids['original'][1]))
-        gdown.download(id=drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
+        gdown.download(drive_ids['original'][0], output=os.path.join(data_folder, drive_ids['original'][1]))
+        gdown.download(drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
     elif args.expr == 'ml':
         data_folder = 'ml_data'
         os.makedirs(data_folder, exist_ok=True)
-        gdown.download(id=drive_ids['original'][0], output=os.path.join(data_folder, drive_ids['original'][1]))
-        gdown.download(id=drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
+        gdown.download(drive_ids['original'][0], output=os.path.join(data_folder, drive_ids['original'][1]))
+        gdown.download(drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
     elif args.expr == 'anon':
         data_folder = 'anon_data'
         os.makedirs(data_folder, exist_ok=True)
-        gdown.download(id=drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
+        gdown.download(drive_ids['arx250_anonymized'][0], output=os.path.join(data_folder, drive_ids['arx250_anonymized'][1]))
     else:
         print("Invalid argument. Experiment type can be [infoloss | dist | ml | anon]")
 
